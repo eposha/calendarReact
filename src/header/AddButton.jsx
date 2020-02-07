bd2a7ab87ccc50c7a49e9714750a835722100cc9
eposha <freedoctime@gmail.com>
2020-02-07 17:07:47 +0300

    fixed

--- src/header/AddButton.jsx
+++ src/header/AddButton.jsx
@@ -1,8 +1,13 @@
 import React from "react";
+import moment from "moment";
 
 const AddButton = ({ showPopup }) => {
+  const todayDate = moment().format("YYYY-MM-DD");
   return (
-    <button className="add-button add-event" onClick={showPopup}>
+    <button
+      className="add-button add-event"
+      onClick={() => showPopup(todayDate, "09:00", "10:00")}
+    >
       <div className="VVsfQ add-event">
         <svg className="add-event" width="36" height="36" viewBox="0 0 36 36">
           <path className="add-event" fill="#34A853" d="M16 16v14h4V20z"></path>
